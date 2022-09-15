db0775a48cc8166a804ade5905ec53652f88c5b6
je9rojas <je9rojas@gmail.com>
2022-09-14 23:54:38 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# request Get method url: "/"
+
+@app.get("/")
+def root():
+    return {"message" : "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+
