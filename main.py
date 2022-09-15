c802e3d2c61a78775053b1b34b7c63af96f15d87
je9rojas <je9rojas@gmail.com>
2022-09-15 00:59:26 -0500

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# request Get method url: "/"
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
 
 @app.get("/")
 def root():
@@ -12,5 +21,8 @@ def root():
 def get_posts():
     return {"data": "This is your posts"}
 
-
-
+@app.post("/createposts")
+def create_posts( post : Post):
+    print(post)
+    print(post.dict())
+    return{ "data" : post }
